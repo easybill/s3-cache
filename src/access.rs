dc08c79d38356d035af3d66ae6dbc36ec3bc63aa
Alexander Klump <alexander.klump@tngtech.com>
2026-03-16 13:47:11 +0100

    add comment about S3Access abuse and refactor

--- src/access.rs
+++ src/access.rs
@@ -8,11 +8,13 @@ pub(crate) struct TelemetryAccess;
 #[async_trait::async_trait]
 impl S3Access for TelemetryAccess {
     async fn check(&self, cx: &mut S3AccessContext<'_>) -> S3Result<()> {
+        // to use this check to record telemetry data is an abuse of the S3Access trait, but we use it here to have a single location where we can record the requested operations
         telemetry::record_endpoint_call(cx.s3_op().name());
 
-        match cx.credentials() {
-            Some(_) => Ok(()),
-            None => Err(s3s::s3_error!(AccessDenied, "Signature is required")),
+        if cx.credentials().is_none() {
+            return Err(s3s::s3_error!(AccessDenied, "Signature is required"));
         }
+
+        Ok(())
     }
 }
